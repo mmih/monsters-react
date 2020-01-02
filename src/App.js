d44234aacb43d4dd75e3f29d373de4a563eb6afe
joelynnturner@gmail.com <joelynnturner@gmail.com>
2020-01-02 18:56:17 +0200

    Inital Commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component'
+import { SearchBox } from './components/search-box/search-box.component'
+import './App.css';
+
+class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      monsters: [],
+      searchFiled: ""
+    }
+  }
+
+  componentWillMount(){
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}))
+  }
+
+  handleChange = (e) => {
+    this.setState({searchFiled: e.target.value})
+  }
+
+  render() {
+
+    const {monsters, searchFiled} = this.state;
+    const filteredMonsters = monsters.filter(monster=>
+      monster.name.toLowerCase().includes(searchFiled.toLowerCase())
+    );
+    
+    return (
+      <div className="App">
+        <h1>Monsters</h1>
+        <SearchBox 
+          placeholder='Search Monsters'
+          handleChange = {this.handleChange}/>
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
